5e827dd6c14b2c73a95726807b36f9dbc7005a5e
Marc Neuwirth <marc.neuwirth@gmail.com>
2012-01-18 15:26:32 +0000

    add cache param

--- dataStorage.js
+++ dataStorage.js
@@ -1,34 +1,37 @@
-var dataStorage = (function() {
+var dataStorage = (function(cache) {
     var data = {},
-        dataQueue = [];
+		dataQueue = [];
 
-    setInterval(function() {
-        var first = dataQueue.shift();
-        if (first && first.key && first.value) {
-            localStorage[first.key] = JSON.stringify(first.value);
-        }
-    }, 200);
+	setInterval(function() {
+		var first = dataQueue.shift();
+		if (first && first.key && first.value) {
+			localStorage[first.key] = JSON.stringify(first.value);
+		}
+	}, 200);
 
-    return {
-        getData: function(key) {
-            if (data[key] !== undefined) {
-                return data[key];
-            }
-            else if (localStorage && localStorage[key] !== undefined) {
-               var obj = JSON.parse(localStorage[key]);
-               data[key] = obj;
-               return obj;
-            }
-            return undefined;
-        },
-        putData: function(key, value) {
-           data[key] = value;
-            if (dataQueue) {
-               dataQueue.push({
-                    key: key,
-                    value: value
-                });
-            }
-        }
-    };
-})();
+	return {
+		getData: function(key) {
+			if (data[key] !== undefined) {
+				return data[key];
+			}
+			else if (localStorage && localStorage[key] !== undefined) {
+				var obj = JSON.parse(localStorage[key]);
+				data[key] = obj;
+				return obj;
+			}
+			return undefined;
+		},
+		putData: function(key, value) {
+			data[key] = value;
+			if (dataQueue) {
+				if(cache){
+					value.timestamp = new Date().setHours(cache).toString();
+				}
+				dataQueue.push({
+					key: key,
+					value: value
+				});
+			}
+		}
+	};
+})('+1 h');
