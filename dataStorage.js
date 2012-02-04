5d2e925088035027b81d8bcd0f847ac8ec217327
Marc Neuwirth <marc.neuwirth@gmail.com>
2012-02-04 21:01:54 +0000

    Reorganized, added removeData method, non-async option

--- dataStorage.js
+++ dataStorage.js
@@ -1,37 +1,63 @@
-var dataStorage = (function(cache) {
-    var data = {},
-		dataQueue = [];
-
-	setInterval(function() {
-		var first = dataQueue.shift();
-		if (first && first.key && first.value) {
-			localStorage[first.key] = JSON.stringify(first.value);
-		}
-	}, 200);
-
-	return {
+var dataStorage = (function(cache, async) {
+    var _ds = {
+		data: {},
+		dataQueue: [],
+		cache: cache || false, //'+1'
+		async: async || true,
+		init: function(){
+			if(_ds.async){
+				setInterval(function() {
+					var first = _ds.dataQueue.shift();
+					if (first && first.key && first.value) {
+						_ds._save(first.key, first.value);
+					}
+				}, 200);
+			}
+		},
+		_save: function(key, value){
+			localStorage[key] = JSON.stringify(value);
+		},
 		getData: function(key) {
-			if (data[key] !== undefined) {
-				return data[key];
+			if (_ds.data[key] !== undefined) {
+				return _ds.data[key];
 			}
 			else if (localStorage && localStorage[key] !== undefined) {
 				var obj = JSON.parse(localStorage[key]);
-				data[key] = obj;
+				_ds.data[key] = obj;
 				return obj;
 			}
 			return undefined;
 		},
 		putData: function(key, value) {
-			data[key] = value;
-			if (dataQueue) {
-				if(cache){
-					value.timestamp = new Date().setHours(cache).toString();
-				}
-				dataQueue.push({
+			_ds.data[key] = value;
+			
+			if(_ds.cache){
+				value.timestamp = new Date().setHours(_ds.cache).toString();
+				console.log(cache, value.timestamp);
+			}
+
+			if(_ds.async){
+				_ds.dataQueue.push({
 					key: key,
 					value: value
 				});
 			}
+			else {
+				_ds._save(key,value);
+			}
+		},
+		removeData: function(key) {
+			delete _ds.data[key];
+			delete localStorage[key];
 		}
 	};
-})('+1');
+
+	_ds.init();
+
+	return {
+		getData: _ds.getData,
+		putData: _ds.putData,
+		removeData: _ds.removeData
+	};
+
+})('+1',true);
